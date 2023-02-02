a8dcbe686e2539ec8cb7a4eb94ed87b891a19616
Raavi Haritha <haritha4682@gmail.com>
2023-02-02 23:11:46 +0530

    About us page completed

--- src/components/Howcanwehelpyou/Howcanwehelpyou.js
+++ src/components/Howcanwehelpyou/Howcanwehelpyou.js
@@ -1,28 +1,28 @@
 import React from "react";
-import './Howcanwehelpyou.scss';
+import "./Howcanwehelpyou.scss";
 import image from "../../images/howcanwehelpimg.jpg";
 
 const Howcanwehelpyou = () => {
-    return (
-      <div className="howcanwehelpyou">
-        <div className="howcanwehelpyou-content">
-            <div className="howcanwehelpyou-content-heading">
-                How can we help you ?
-            </div>
-            <hr class="howcanwehelpyou-heading-ul"></hr>
-            <div className="howcanwehelpyou-content-paragraph">
-            Beyond Syntax LLC is founded to serve its customers with a core set of values. 
-            These core values guide our actions as we engage with our customers, employees, partners, and 
-            all whom we serve. We believe strongly in the value of high-quality processes to deliver 
-            consistent results, and a continuous improvement process to ensure we can be nimble and adopt 
-            quickly to technology, industry and market change 
-            </div>
+  return (
+    <div className="howcanwehelpyou">
+      <div className="howcanwehelpyou-content">
+        <div className="howcanwehelpyou-content-heading">
+          How can we help you ?
         </div>
-        <div className="howcanwehelpyou-image">
-            <img src={image} alt="image" />
+        <hr class="howcanwehelpyou-heading-ul"></hr>
+        <div className="howcanwehelpyou-content-paragraph">
+          Fountain Tech's commitment to client success is evident in its high
+          client retention rate and the long-term partnerships it builds with
+          its clients. Whether you're a small business or a large enterprise,
+          Fountain Tech has the skills and experience to help you unlock the
+          full potential of Salesforce and drive your business forward.
         </div>
       </div>
-    );
+      <div className="howcanwehelpyou-image">
+        <img src={image} alt="image" />
+      </div>
+    </div>
+  );
 };
 
 export default Howcanwehelpyou;
