e8dbb1043af2426c56a0285babe001a629c64af8
AmruthaAnumula <ammuamrutha35423@gmail.com>
2023-02-05 00:38:59 +0530

    changes are done.

--- src/components/index.js
+++ src/components/index.js
@@ -8,7 +8,6 @@ import Howcanwehelpyou from "./Howcanwehelpyou/Howcanwehelpyou";
 import Ourtechnologies from "./Ourtechnologies/Ourtechnologies";
 import Ourclients from "./Ourclients/Ourclients";
 import Aboutbanner from "./Aboutusbanner/Aboutusbanner";
-
 import Visionmission from "./Visionmission/Visionmission";
 import Servicesbanner from "./Servicesbanner/Servicesbanner";
 import Itstaffingcontent from "./Itstaffingcontent/Itstaffingcontent";
@@ -17,15 +16,9 @@ import RpoMps from "./RpoMps/RpoMps";
 import Contactusbanner from "./Contactusbanner/Contactusbanner";
 import Contactuscontent from "./Contactuscontent/Contactuscontent";
 import Contactusform from "./Contactusform/Contactusform";
-<<<<<<< HEAD
-import ImageTextComponent from "./ImageTextComponent/ImageTextComponent";
-export { Header, Footer, Imagecarousel, Ourservices, Aboutbeyondsyntax, Whywithus, Howcanwehelpyou, Ourtechnologies, Ourclients, Aboutbanner, Whypartnerwithus, Visionmission, Itstaffingbanner, Itstaffingcontent, Weoffer, RpoMps, Contactusbanner, Contactuscontent, Contactusform,ImageTextComponent};
-=======
 import TextComponent from "./TextComponent/TextComponent";
+import ImageTextComponent from "./ImageTextComponent/ImageTextComponent";
 
-<<<<<<< HEAD
-export { Header, Footer,TextComponent, Imagecarousel, Ourservices, AboutFountainTech, Whywithus, Howcanwehelpyou, Ourtechnologies, Ourclients, Aboutbanner, Visionmission, Servicesbanner, Itstaffingcontent, Weoffer, RpoMps, Contactusbanner, Contactuscontent, Contactusform,};
-=======
-export { Header, Footer,TextComponent, Imagecarousel, Ourservices, AboutFountainTech, Whywithus, Howcanwehelpyou, Ourtechnologies, Ourclients, Aboutbanner, Visionmission, Itstaffingbanner, Itstaffingcontent, Weoffer, RpoMps, Contactusbanner, Contactuscontent, Contactusform,};
->>>>>>> bd1709c40d463b7af36350220bfeb3f6cf899005
->>>>>>> 3dbbd1bc66bdfea4f2a4d08f80252c8c57d0488c
+
+export { Header, Footer,TextComponent, Imagecarousel, Ourservices, AboutFountainTech, Whywithus, Howcanwehelpyou, Ourtechnologies, Ourclients, Aboutbanner, Visionmission, Servicesbanner, Itstaffingcontent, Weoffer, RpoMps, Contactusbanner, Contactuscontent, Contactusform,ImageTextComponent};
+
